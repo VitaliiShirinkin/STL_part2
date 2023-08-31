a8dce0d9f62ee0f66a5047224af81eca719c91df
Vitalii Shirinkin <artoiv2@mail.ru>
2023-08-31 18:47:15 +0300

    Initial

---
+++ HW1/HW1.cpp
@@ -0,0 +1,29 @@
+﻿// HW1.cpp : Задача 1. Уникальный вектор
+//Нужно реализовать функцию, которая удаляет все дубликаты, а не только подряд идущие, из std::vector.
+
+#include <iostream>
+#include<vector>
+#include<algorithm>
+
+int main()
+{
+    auto print = [](const std::vector<int>& V) {
+        for (const auto& i : V) { std::cout << i << " "; }
+        std::cout << "\n\n"; };
+
+    std::cout << "[IN]: "<<'\n';
+    std::vector<int>vec {1, 1, 2, 5, 6, 1, 2, 4};
+    print(vec);
+
+    std::cout << "After sort: " << '\n';
+    std::sort(vec.begin(), vec.end());
+    print(vec);
+
+    std::cout << "[OUT]: " << '\n';
+    auto U = std::unique(vec.begin(), vec.end());
+    vec.erase(U, vec.end());
+    print(vec);
+    return 0;
+}
+
+
---
+++ HW2/HW2.cpp
@@ -0,0 +1,26 @@
+﻿// HW2.cpp : Задача 2. Печать контейнера
+
+#include <iostream>
+#include<vector>
+#include<set>
+#include<list>
+
+template<class T>
+void print_container(const T& container) {
+    for (auto it = container.begin(); it != container.end(); ++it)
+        std::cout << *it << "\t";
+    std::cout<<std::endl;
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set); // four one three two
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list); // one, two, three, four
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector); // one, two, three, four
+}
+
